1815b34d623a8d55a1b3f4d6e87efad079570760
Thomas Bisset <5402845+ThomasBisset@users.noreply.github.com>
2023-04-01 10:53:42 +0000

    Initial commit, random class, age and height.

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+import random
+
+
+def random_class():
+    class_list = ['fighter', 'invoker', 'ranger', 'naturalist', 'doctor', 'spy', 'magician', 'wizard']
+    return random.choice(class_list)
+
+
+name = input("What is your name?:   ")
+pronouns = input("What is your pronouns?:   ")
+
+print("Hello,")
+print(f"My name is {name} ({pronouns}).")
+print(f"I am {random.randrange(2, 300, 1)} years old and stand {random.randrange(3, 8, 1)} feet tall.")
+print(f"I'm the party's {random_class()}.")
 
